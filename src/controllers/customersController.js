70e69a2967e11d6ef1acca38d0a8810e956e24d1
Joao Paulo <oliveirajpufrj@gmail.com>
2022-05-30 11:52:54 -0300

    feat: get single costumer

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -21,3 +21,20 @@ try {
     res.sendStatus(500);
 }
 }
+
+export const getSingleCustomer = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const query = await db.query(`SELECT * FROM customers WHERE id=$1`, [id])
+        const customer = query.rows[0];
+        if(customer.length===0){
+            return res.status(404).send('User not found!');
+        }
+
+        res.status(200).send(customer);
+    } catch (error) {
+        console.log("error get single costumer: ", error);
+        res.sendStatus(500)
+    }
+}
